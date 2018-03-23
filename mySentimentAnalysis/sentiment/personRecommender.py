f04eb1ca193735581957401b49f05e49b953ccf0
katarina <katarina.veleglavac@fer.hr>
2018-03-23 21:44:50 +0100

    Rad na sentiment-analizi. Topic Modeling pocetni commmit.

--- mySentimentAnalysis/sentiment/personRecommender.py
+++ mySentimentAnalysis/sentiment/personRecommender.py
@@ -12,6 +12,7 @@ import os
 import nltk
 import glob
 
+
 path = "C:/Users/Katarina123/workspace/mySpider/spiders/cleanTextFromHTML/"
 
 for fileName in glob.glob(os.path.join(path, '*.txt')):
@@ -35,20 +36,21 @@ for fileName in glob.glob(os.path.join(path, '*.txt')):
     #print (cleanedTextFromPage)
     text = Text(cleanedTextFromPage, hint_language_code='hr')
     person = []
-    for sent in text.sentences:
-        #print(sent, "\n")
-        for entity in sent.entities:
-            #print (entity)
-            if(entity.tag == "I-PER"):
+    if(text):
+        for sent in text.sentences:
+            #print(sent, "\n")
+            for entity in sent.entities:
                 #print (entity)
-                if (len(entity) > 1):
-                    name = ""
-                    for en in entity:
-                        #print (en)
-                        name = name + en + " "
+                if(entity.tag == "I-PER"):
+                    #print (entity)
+                    if (len(entity) > 1):
+                        name = ""
+                        for en in entity:
+                            #print (en)
+                            name = name + en + " "
                         
-                    name = name.strip(" ")
-                    if(name not in person):
-                        person.append(name)
+                        name = name.strip(" ")
+                        if(name not in person):
+                            person.append(name)
                     
-    print (person)
+        print (person)
